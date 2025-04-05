4ee2102dee63766eb4fdb4786fa447971e5c1a34
Przemek Stepien <PrzemekTGK@gmail.com>
2025-04-05 23:52:49 +0100

    vite.config.js update for test deployment

--- mern/client/vite.config.js
+++ mern/client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // ensure the build output is set to the 'dist' folder
+  },
+  server: {
+    // Disable the default localhost binding, allowing the server to be exposed externally
+    host: true,
+    port: 5173, // You can change this if needed
+    strictPort: true,
+    open: false, // You can set this to true if you want to automatically open the browser on start
+  },
+});
