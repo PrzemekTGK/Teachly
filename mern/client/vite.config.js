3ec1f7a91ebb504415be5b581cb0db17b3f09350
Przemek Stepien <PrzemekTGK@gmail.com>
2025-04-06 19:48:54 +0100

    deploy fix check

--- mern/client/vite.config.js
+++ mern/client/vite.config.js
@@ -1,20 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // ensure the build output is set to the 'dist' folder
-    rollupOptions: {
-      input: "/src/main.jsx", // Ensure this is pointing to your entry file (usually main.js or index.js)
-    },
+    outDir: "dist",
   },
   server: {
-    // Disable the default localhost binding, allowing the server to be exposed externally
     host: true,
-    port: 5173, // You can change this if needed
+    port: 5173,
     strictPort: true,
-    open: false, // You can set this to true if you want to automatically open the browser on start
+    open: false,
   },
 });
