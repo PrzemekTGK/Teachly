276f6eeb49fca3c3960c627f6d5f1d2d38199881
Przemek Stepien <PrzemekTGK@gmail.com>
2025-04-06 18:30:26 +0100

    Stream fix check

--- mern/server/controllers/streamController.js
+++ mern/server/controllers/streamController.js
@@ -2,12 +2,26 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 export const streamProxy = async (req, res, next) => {
   console.log("Stream proxy triggered for:", req.url);
-
-  createProxyMiddleware({
-    target: "https://ec2-51-21-152-36.eu-north-1.compute.amazonaws.com/hls",
+  const proxy = createProxyMiddleware({
+    target: "http://ec2-51-21-152-36.eu-north-1.compute.amazonaws.com",
     changeOrigin: true,
     pathRewrite: {
-      "^/api/stream": "",
+      "^/api/stream/hls": "/hls",
     },
-  })(req, res, next);
+    onProxyRes: (proxyRes, req, res) => {
+      if (req.url.endsWith(".m3u8")) {
+        let body = "";
+        proxyRes.on("data", (chunk) => (body += chunk));
+        proxyRes.on("end", () => {
+          const rewritten = body.replace(
+            /http:\/\/ec2-51-21-152-36\.eu-north-1\.compute\.amazonaws\.com\/hls/g,
+            `https://${req.headers.host}/api/stream/hls`
+          );
+          res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
+          res.end(rewritten);
+        });
+      }
+    },
+  });
+  proxy(req, res, next);
 };
