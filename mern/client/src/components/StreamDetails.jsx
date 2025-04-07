fa4c768d0390e4bae7adb23d49482e917ebb96fa
Przemek Stepien <PrzemekTGK@gmail.com>
2025-04-07 23:04:50 +0100

    Stream details update

--- mern/client/src/components/StreamDetails.jsx
+++ mern/client/src/components/StreamDetails.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 export default function StreamDetails() {
+  const [streamState, setStreamState] = useState({
+    streamTitle: "",
+    Streamdescription: "",
+  });
+
+  const [error, setError] = useState(""); // For error messages
+  const [success, setSuccess] = useState("");
+
+  function updateHandler(e) {
+    setStreamState({ ...streamState, [e.target.name]: e.target.value });
+  }
+
+  function handlePublishStream() {}
+
   return (
     <div className="stream-deails">
       <h2>Stream Details</h2>
+      <form onSubmit={handlePublishStream}>
+        <input
+          className="stream-title-input"
+          placeholder="Stream Title"
+          onChange={updateHandler}
+          name="text"
+          required
+          maxLength={40}
+        />
+        <textarea
+          className="upload-video-decription"
+          placeholder="Description"
+          value={streamState}
+          onChange={(e) => streamState(e.target.value)}
+        />
+        <button className="submit-button" type="submit">
+          Punlish Stream
+        </button>
+      </form>
+      {/* Show success or error messages */}
+      {error && <p className="error-message">{error}</p>}
+      {success && <p className="success-message">{success}</p>}
     </div>
   );
 }
