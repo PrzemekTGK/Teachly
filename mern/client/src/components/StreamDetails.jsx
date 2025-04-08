8f14180a0bad38af0056c4fd70de27bfaffaa5a7
Przemek Stepien <PrzemekTGK@gmail.com>
2025-04-08 15:40:38 +0100

    css update

--- mern/client/src/components/StreamDetails.jsx
+++ mern/client/src/components/StreamDetails.jsx
@@ -16,36 +16,34 @@ export default function StreamDetails() {
   function handlePublishStream() {}
 
   return (
-    <div className="stream-details-container">
-      <div className="stream-details">
-        <form className="stream-details-form" onSubmit={handlePublishStream}>
-          <label>Stream Title</label>
-          <input
-            className="stream-title-input"
-            placeholder="Stream Title"
-            onChange={updateHandler}
-            name="text"
-            value={streamDetails.streamTitle}
-            required
-            maxLength={40}
-          />
-          <label>Stream Description</label>
-          <textarea
-            className="stream-description"
-            placeholder="Stream Description"
-            value={streamDetails.streamDescription}
-            onChange={updateHandler}
-            required
-            maxLength={150}
-          />
-          <button className="publish-stream-button" type="submit">
-            Publish Stream
-          </button>
-        </form>
-        {/* Show success or error messages */}
-        {error && <p className="error-message">{error}</p>}
-        {success && <p className="success-message">{success}</p>}
-      </div>
+    <div className="stream-details">
+      <form className="stream-details-form" onSubmit={handlePublishStream}>
+        <label>Stream Title</label>
+        <input
+          className="stream-title-input"
+          placeholder="Stream Title"
+          onChange={updateHandler}
+          name="text"
+          value={streamDetails.streamTitle}
+          required
+          maxLength={40}
+        />
+        <label>Stream Description</label>
+        <textarea
+          className="stream-description"
+          placeholder="Stream Description"
+          value={streamDetails.streamDescription}
+          onChange={updateHandler}
+          required
+          maxLength={150}
+        />
+        <button className="publish-stream-button" type="submit">
+          Publish Stream
+        </button>
+      </form>
+      {/* Show success or error messages */}
+      {error && <p className="error-message">{error}</p>}
+      {success && <p className="success-message">{success}</p>}
     </div>
   );
 }
