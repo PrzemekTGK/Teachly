57f9abb6bd469644db2700ac65cbf5351af13875
Przemek Stepien <PrzemekTGK@gmail.com>
2025-04-08 02:34:58 +0100

    css optimalization update

--- mern/client/src/components/StreamDetails.jsx
+++ mern/client/src/components/StreamDetails.jsx
@@ -1,37 +1,39 @@
 import { useState } from "react";
 
 export default function StreamDetails() {
-  const [streamState, setStreamState] = useState({
+  const [streamDetails, setStreamState] = useState({
     streamTitle: "",
-    Streamdescription: "",
+    streamDescription: "",
   });
 
   const [error, setError] = useState(""); // For error messages
   const [success, setSuccess] = useState("");
 
   function updateHandler(e) {
-    setStreamState({ ...streamState, [e.target.name]: e.target.value });
+    setStreamState({ ...streamDetails, [e.target.name]: e.target.value });
   }
 
   function handlePublishStream() {}
 
   return (
     <div className="stream-details">
-      <h2>Stream Details</h2>
       <form className="stream-details-form" onSubmit={handlePublishStream}>
+        <label>Stream Title</label>
         <input
           className="stream-title-input"
           placeholder="Stream Title"
           onChange={updateHandler}
           name="text"
+          value={streamDetails.streamTitle}
           required
           maxLength={40}
         />
+        <label>Stream Description</label>
         <textarea
           className="stream-description"
           placeholder="Stream Description"
-          value={streamState}
-          onChange={(e) => streamState(e.target.value)}
+          value={streamDetails.streamDescription}
+          onChange={updateHandler}
           required
           maxLength={150}
         />
