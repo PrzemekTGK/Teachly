9d70470c8266742a6d300c08f0e3f97cd1373d2b
Przemek Stepien <PrzemekTGK@gmail.com>
2025-04-11 21:55:32 +0100

    debug update

--- mern/server/websocket.js
+++ mern/server/websocket.js
@@ -1,20 +1,15 @@
 import { WebSocketServer } from "ws";
 
 export function initializeWebSocket(server) {
-  console.log("Creating WebSocket Server!");
   const wss = new WebSocketServer({ server });
-  console.log("WebSocket server created");
   const clients = new Map();
 
   wss.on("connection", (ws) => {
-    console.log("Client connected");
     ws.on("message", (message) => {
       try {
         const data = JSON.parse(message.toString());
         if (data.streamKey) {
           clients.set(data.streamKey, ws);
-          console.log(`Registered ${data.streamKey}, clients: ${clients.size}`);
-          console.log(`CLIENTS: `, clients);
         }
       } catch (error) {
         console.error("WebSocket message error:", error);
