2a63a6b55b46771853bef8ca7b181f9ef880ce9b
Przemek Stepien <PrzemekTGK@gmail.com>
2025-04-26 17:45:13 +0100

    CORS debugging

--- mern/server/server.js
+++ mern/server/server.js
@@ -38,7 +38,7 @@ app.use(
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
     credentials: true,
   })
 );
