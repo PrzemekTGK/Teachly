333d5c5a840d191616166a02a3b0f68ffbbc12f4
Przemek Stepien <PrzemekTGK@gmail.com>
2025-04-26 17:54:50 +0100

    CORS debugging

--- mern/server/server.js
+++ mern/server/server.js
@@ -20,22 +20,33 @@ const server = createServer(app);
 const clients = initializeWebSocket(server);
 app.set("wssClients", clients);
 
+app.use((req, res, next) => {
+  console.log(`[REQUEST] ${req.method} ${req.url}`);
+  next();
+});
+
 app.use(
   cors({
     origin: "https://teachly.up.railway.app",
     methods: ["GET", "HEAD", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
+    credentials: true,
   })
 );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(upload.any());
+//app.use(upload.any());
 
+app.use("/api/images", upload.any(), awsImgRouter);
+app.use("/api/videos", upload.any(), awsVidRouter);
 app.use("/api/users", userRouter);
-app.use("/api/images", awsImgRouter);
-app.use("/api/videos", awsVidRouter);
 app.use("/api/stream", streamRouter);
+
+app.use((err, req, res, next) => {
+  console.error(`[SERVER ERROR] ${err.message}`);
+  res.status(500).json({ message: "Internal Server Error" });
+});
 
 server.listen(PORT, "0.0.0.0", () => {
   connectDB();
